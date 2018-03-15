b9ebec104b90db1af9c945fa4ba296eecba5005f
ws <wllmshao@gmail.com>
2018-03-15 00:55:06 -0700

    Dummy training script

---
+++ training.py
@@ -0,0 +1,33 @@
+import feather
+import pandas as pd
+from sklearn.svm import LinearSVC
+
+df = feather.read_dataframe('./data/final_feathers/total_df.feather')
+print 'read df'
+
+train_y = df[df.fold==0].truth.values
+test_y = df[df.fold==2].truth.values
+
+train_X = df[df.fold==0]
+test_X = df[df.fold==2]
+
+del train_X['truth']
+del train_X['fold']
+
+del test_X['truth']
+del test_X['fold']
+
+svc = LinearSVC(dual=False,penalty='l1',C=0.5,random_state=1)
+
+print 'starting fit'
+
+svc.fit(train_X, train_y)
+
+print 'finished fit'
+
+print 'starting score'
+
+score = svc.score(test_X, test_y)
+
+print 'finished score'
+print score
