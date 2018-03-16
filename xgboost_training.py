5c99af22e12c525dd76eac968f6751813e3b29af
ws <wllmshao@gmail.com>
2018-03-16 11:07:40 -0700

    xgboost_training.py added

---
+++ xgboost_training.py
@@ -0,0 +1,49 @@
+import xgboost as xgb
+import feather
+import pandas as pd
+import numpy as np
+import scipy.sparse as sp
+from sklearn.metrics import roc_auc_score
+
+print 'reading feather'
+df = feather.read_dataframe('./data/final_feathers/total_df.feather')
+
+print 'reading pickle'
+str_column = pd.read_pickle('./data/final_feathers/string_pickle.bin')
+
+vec = HashingVectorizer(dtype=np.uint8, n_features=500000,
+    norm=None, lowercase=False, binary=True, token_pattern='\S+', 
+    non_negative=True)
+
+print 'getting ys'
+y_train = df[df.fold<=1].truth.values
+y_test = df[df.fold==2].truth.values
+
+print 'creating Xs'
+X_train = vec.transform(str_column[df.fold<=1])
+print 'X_train done'
+X_test = vec.transform(str_column[df.fold==2])
+print 'Xs done'
+
+del df
+del str_column
+
+print 'creating DMatrix'
+dtrain = xgb.DMatrix(X_train, label=y_test)
+
+print 'training'
+param = {'n_jobs': 7}
+bst = xgb.train(param, dtrain, verbost_eval=True)
+
+print 'done training'
+
+print 'saving model'
+bst.save_model('./models/xgb.model')
+
+print 'predicting y_test'
+y_pred = bst.predict(y_test)
+
+print 'getting roc'
+auc = roc_auc_score(y_test, y_pred)
+
+print auc
